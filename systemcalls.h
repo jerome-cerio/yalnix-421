b69177aaab89e1d3a60936ba2712314bb5d8143f
Jerome Cerio <112119089+jerome-cerio@users.noreply.github.com>
2024-03-28 14:30:15 -0500

    Create systemcalls.h
    header file for system calls. just prototypes

---
+++ systemcalls.h
@@ -0,0 +1,20 @@
+#ifndef SYSTEMCALLS_H
+#define SYSTEMCALLS_H
+
+
+#include <comp421/hardware.h>
+
+
+// System call handler prototypes
+int KernelFork (int );
+int KernelExec (char* , char** , ExceptionInfo* );
+void KernelExit (int );
+int KernelWait (int* );
+int KernelGetPid ();
+int KernelBrk (void* );
+int KernelDelay (int );
+int KernelTtyRead (int , void* , int );
+int KernelTtyWrite (int , void* , int );
+
+
+#endif
